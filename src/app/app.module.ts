58ae8e1f975a7c50f17cc21ede13dc6272702e9f
Ani <aniruddhajoshi12@gmail.com>
2024-03-09 15:30:21 +0530

    about and portfolio added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,29 @@ import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
 import { AboutComponent } from './about/about.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ServicesComponent } from './services/services.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [{
+  // path:'/admin',
+  // loadChildren: () =>
+  // import('./admin/admin.module').then(m => m.AdminModule)
+}];
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BannerComponent,
-    AboutComponent
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
