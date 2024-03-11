205c19317162573b5f7ec0aab91d1af676cbfc4b
Ani <aniruddhajoshi12@gmail.com>
2024-03-11 23:36:24 +0530

    nav bar added

--- src/app/admin/post-login/post-login-main/post-login-main.component.ts
+++ src/app/admin/post-login/post-login-main/post-login-main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@app/shared/auth.service';
 
 @Component({
   selector: 'app-post-login-main',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostLoginMainComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth:AuthService,
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
+  }
+  logout(){
+    this.auth.logout();
+    this.router.navigate(['/admin']);
   }
 
 }
