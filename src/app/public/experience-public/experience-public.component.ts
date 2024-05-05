6f6f5fbf2587b8afb52b465eb796e39ce3453895
Ani <aniruddhajoshi12@gmail.com>
2024-05-05 14:01:47 +0530

    experience tab changes

--- src/app/public/experience-public/experience-public.component.ts
+++ src/app/public/experience-public/experience-public.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonServiceService } from '@app/shared/common-service.service';
 
 @Component({
   selector: 'app-experience-public',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperiencePublicComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    private commonService:CommonServiceService
+  ) { }
+  data:any;
   ngOnInit(): void {
+    this.getExperience()
+  }
+  getExperience(){
+    this.commonService.getApi('https://fastapi-app-hf22.onrender.com/get-exp').subscribe((res)=>{
+    this.data=res;
+    })
   }
 
 }
